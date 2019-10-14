d55ddddd65d92f61692c47988e248f5caf7cead5
Lucas Jorge <lucas1jorge@gmail.com>
2019-10-14 09:10:22 -0300

    Add files via upload

--- jig_saw.py
+++ jig_saw.py
@@ -3,11 +3,15 @@ from PIL import Image
 from math import inf
 
 class neighbor:
-    def __init__(self, l, d, r, u):
-        self.up = u
-        self.down = d
+    def __init__(self, l, d, r, u, l_cost, d_cost, r_cost, u_cost):
         self.left = l
+        self.down = d
         self.right = r
+        self.up = u
+        self.l_cost = inf
+        self.d_cost = inf
+        self.r_cost = inf
+        self.u_cost = inf
 
 def cmp_left(a, b, p):
     ans = 0
@@ -16,9 +20,9 @@ def cmp_left(a, b, p):
             curr_a = a[i][0][j]
             curr_b = b[i][p - 1][j]
             if curr_a > curr_b:
-                ans += curr_a - curr_b
+                ans += (curr_a - curr_b)**2
             else:
-                ans += curr_b - curr_a
+                ans += (curr_b - curr_a)**2
     return ans
 
 def cmp_down(a, b, p):
@@ -28,9 +32,9 @@ def cmp_down(a, b, p):
             curr_a = a[p - 1][i][j]
             curr_b = b[0][i][j]
             if curr_a > curr_b:
-                ans += curr_a - curr_b
+                ans += (curr_a - curr_b)**2
             else:
-                ans += curr_b - curr_a
+                ans += (curr_b - curr_a)**2
     return ans
 
 def cmp_right(a, b, p):
@@ -40,9 +44,9 @@ def cmp_right(a, b, p):
             curr_a = a[i][p - 1][j]
             curr_b = b[i][0][j]
             if curr_a > curr_b:
-                ans += curr_a - curr_b
+                ans += (curr_a - curr_b)**2
             else:
-                ans += curr_b - curr_a
+                ans += (curr_b - curr_a)**2
     return ans
 
 def cmp_up(a, b, p):
@@ -52,13 +56,88 @@ def cmp_up(a, b, p):
             curr_a = a[0][i][j]
             curr_b = b[p - 1][i][j]
             if curr_a > curr_b:
-                ans += curr_a - curr_b
+                ans += (curr_a - curr_b)**2
             else:
-                ans += curr_b - curr_a
+                ans += (curr_b - curr_a)**2
+    return ans
+
+def expand(grid, index, last, hor, ver, m, p, x):
+    line = [None] * m
+    _i = last[0]
+    _j = last[1]
+
+    curr = [_i, _j]
+    j = _j
+    for i in range(_i, _i + m * ver, ver):
+        line[i - _i] = curr
+
+        index[i][j] = (curr[0], curr[1])
+        grid[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
+
+        Next = None
+        if ver == 1:
+            Next = neighbors[curr[0]][curr[1]].down
+        elif ver == -1:
+            Next = neighbors[curr[0]][curr[1]].up
+        curr = Next
+
+    for j in range(_j + hor, _j + m * hor, hor):
+        for i in range(_i, _i + m * ver, ver):
+            prev = line[i - _i]
+            neighbor = neighbors[prev[0]][prev[1]]
+            if hor == 1:
+                curr = neighbor.right
+            elif hor == -1:
+                curr = neighbor.left
+
+            index[i][j] = (curr[0], curr[1])
+            grid[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
+
+            line[i - _i] = curr
+
+def correct(grid, index, cost, neighbors, last, hor, ver):
+    _i = last[0]
+    _j = last[1]
+
+    j = _j
+    for i in range(_i + hor, _i + m * hor, hor):
+        curr_min = inf
+        i_ind = index[i]
+        j_ind = index[j]
+        # curr_min = min(curr_min, cot)
+
+def get_min_cost(cost, m):
+    ans = [0, 0]
+    ans_cost = inf
+
+    for i in range(len(cost)):
+        for j in range(len(cost[i])):
+            if i > 0:
+                cost[i][j] += cost[i - 1][j]
+            if j > 0:
+                cost[i][j] += cost[i][j - 1]
+            if i > 0 and j > 0:
+                cost[i][j] -= cost[i - 1][j - 1]
+
+    for i in range(m - 1, len(cost)):
+        for j in range(m - 1, len(cost[i])):
+            curr = cost[i][j]
+
+            if i >= m:
+                curr -= cost[i - m][j]
+            if j >= m:
+                curr -= cost[i][j - m]
+            if i >= m and j >= m:
+                curr += cost[i - m][j - m]
+
+            if curr < ans_cost:
+                ans_cost = curr
+                ans = [i - m + 1, j - m + 1]
+    
     return ans
 
 if __name__ == "__main__":
-    img_index = "5"
+    img_index = "4"
 
     # problem A
     p = 64
@@ -148,99 +227,48 @@ if __name__ == "__main__":
                 max_r_u= sum_r_u
                 r_u = [i, j, 3]
 
-            neighbors[i][j] = neighbor(curr_left, curr_down, curr_right, curr_up)
+            neighbors[i][j] = neighbor(curr_left, curr_down, curr_right, curr_up, left_min, down_min, right_min, up_min)
 
 
-    ans = np.copy(x)
+    grid = np.copy(x)
+    grid = np.concatenate((grid, grid), axis=0)
+    grid = np.concatenate((grid, grid), axis=1)
+    
+    index = [[None for j in range(2 * m - 1)] for i in range(2 * m - 1)]
+
+    last = [m - 1, m - 1]
 
     # propagate from top-left
-    line = [None] * m
-    i = 0
-    curr = [l_u[0], l_u[1]]
-
-    j = 0
-    for i in range(m):
-        line[i] = curr
-        ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-        down = neighbors[curr[0]][curr[1]].down
-        curr = down
-
-    for j in range(1, m):
-        for i in range(m):
-            prev = line[i]
-            curr = neighbors[prev[0]][prev[1]].right
-            ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-
-            line[i] = curr
-
-    img = Image.fromarray(ans, 'RGB')
+    expand(grid, index, last, 1, 1, m, p, x)
+    img = Image.fromarray(grid, 'RGB')
     img.save(img_index + "_restored_0.jpeg")
 
-    # Propagate from bottom-left
-    line = []
-    i = m - 1
-    curr = [l_d[0], l_d[1]]
-    line.append(curr)
-
-    for j in range(m):
-        ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-        right = neighbors[curr[0]][curr[1]].right
-        curr = right
-        line.append(curr)
-
-    for i in range(m - 2, -1, -1):
-        for j in range(m):
-            prev = line[j]
-            curr = neighbors[prev[0]][prev[1]].up
-            ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-
-            line[j] = curr
-
-    img = Image.fromarray(ans, 'RGB')
+    # propagate from bottom-left
+    expand(grid, index, last, 1, -1, m, p, x)
+    img = Image.fromarray(grid, 'RGB')
     img.save(img_index + "_restored_1.jpeg")
 
-    # Propagate from bottom-right
-    line = [None] * m
-    i = m - 1
-    curr = [r_d[0], r_d[1]]
-
-    for j in range(m - 1, -1, -1):
-        ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-        
-        line[j] = curr
-        left = neighbors[curr[0]][curr[1]].left
-        curr = left
-
-    for i in range(m - 2, -1, -1):
-        for j in range(m - 1, -1, -1):
-            prev = line[j]
-            curr = neighbors[prev[0]][prev[1]].up
-            ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-
-            line[j] = curr
-
-    img = Image.fromarray(ans, 'RGB')
+    # propagate from top-right
+    expand(grid, index, last, -1, 1, m, p, x)
+    img = Image.fromarray(grid, 'RGB')
     img.save(img_index + "_restored_2.jpeg")
 
-    # Propagate from top-right
-    line = [None] * m
-    i = m - 1
-    curr = [r_u[0], r_u[1]]
-    line[m - 1] = curr
+    # propagate from bottom-right
+    expand(grid, index, last, -1, -1, m, p, x)
+    img = Image.fromarray(grid, 'RGB')
+    img.save(img_index + "_restored_3.jpeg")
 
-    for j in range(m - 2, -1, -1):
-        ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
-        right = neighbors[curr[0]][curr[1]].right
-        curr = right
-        line[j] = curr
+    cost = [[0 for j in range(2 * m - 1)] for i in range(2 * m - 1)]
 
-    for i in range(m - 2, -1, -1):
-        for j in range(m - 1, -1, -1):
-            prev = line[j]
-            curr = neighbors[prev[0]][prev[1]].up
-            ans[p*i:p*(i+1), p*j:p*(j+1)] = x[p*curr[0]:p*(curr[0]+1), p*curr[1]:p*(curr[1]+1)]
+    correct(grid, index, cost, neighbors, last, 1, 1)
+    correct(grid, index, cost, neighbors, last, 1, -1)
+    correct(grid, index, cost, neighbors, last, -1, 1)
+    correct(grid, index, cost, neighbors, last, -1, -1)
+    
+    [i, j] = get_min_cost(cost, m)
 
-            line[j] = curr
+    ans = grid[i : i + p * m, j : j + p * m]
+    # ans = grid
 
     img = Image.fromarray(ans, 'RGB')
-    img.save(img_index + "_restored_3.jpeg")
+    img.save(img_index + "_ans.jpeg")
---
+++ jigsaw_MST_16.py
@@ -0,0 +1,277 @@
+import numpy as np
+from PIL import Image
+from math import inf
+import heapq
+
+def index(i, j, m):
+    return m * i + j
+
+def inv_index(i, m):
+    _i = i // m
+    _j = i % m
+    return [_i, _j]
+
+def direction(d):
+    if d == "u": return [0, -1]
+    if d == "l": return [-1, 0]
+    if d == "r": return [1, 0]
+    if d == "d": return [0, 1]
+
+def cmp_left(a, b, p):
+    ans = 0
+    for i in range(p):
+        for j in range(3):
+            curr_a = a[i][0][j]
+            curr_b = b[i][p - 1][j]
+            if curr_a > curr_b:
+                ans += curr_a - curr_b
+            else:
+                ans += curr_b - curr_a
+    return ans
+
+def cmp_down(a, b, p):
+    ans = 0
+    for i in range(p):
+        for j in range(3):
+            curr_a = a[p - 1][i][j]
+            curr_b = b[0][i][j]
+            if curr_a > curr_b:
+                ans += curr_a - curr_b
+            else:
+                ans += curr_b - curr_a
+    return ans
+
+def cmp_right(a, b, p):
+    ans = 0
+    for i in range(p):
+        for j in range(3):
+            curr_a = a[i][p - 1][j]
+            curr_b = b[i][0][j]
+            if curr_a > curr_b:
+                ans += curr_a - curr_b
+            else:
+                ans += curr_b - curr_a
+    return ans
+
+def cmp_up(a, b, p):
+    ans = 0
+    for i in range(p):
+        for j in range(3):
+            curr_a = a[0][i][j]
+            curr_b = b[p - 1][i][j]
+            if curr_a > curr_b:
+                ans += curr_a - curr_b
+            else:
+                ans += curr_b - curr_a
+    return ans
+
+def get_min_cost(cost, m):
+    ans = [0, 0]
+    ans_cost = inf
+
+    for i in range(len(cost)):
+        for j in range(len(cost[i])):
+            if i > 0:
+                cost[i][j] += cost[i - 1][j]
+            if j > 0:
+                cost[i][j] += cost[i][j - 1]
+            if i > 0 and j > 0:
+                cost[i][j] -= cost[i - 1][j - 1]
+
+    for i in range(m - 1, len(cost)):
+        for j in range(m - 1, len(cost[i])):
+            curr = cost[i][j]
+
+            if i >= m:
+                curr -= cost[i - m][j]
+            if j >= m:
+                curr -= cost[i][j - m]
+            if i >= m and j >= m:
+                curr += cost[i - m][j - m]
+
+            if curr < ans_cost:
+                ans_cost = curr
+                ans = [i - m + 1, j - m + 1]
+    
+    return ans
+
+class edge:
+    def __init__(self, u, v, w, src, dest):
+        self.u = u
+        self.v = v
+        self.w = w
+        self.src = src
+        self.dest = dest
+
+class cell:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+        self.children = {}
+
+def get_edges(x, p, m, edges_matrix):
+    edges = []
+
+    for i in range(m):
+        for j in range(m):
+            curr = x[p*i : p*(i + 1), p*j : p*(j + 1)]
+
+            for _i in range(m):
+                for _j in range(m):
+                    if _i == i and _j == j: continue
+
+                    _curr = x[p*_i : p*(_i + 1), p*_j : p*(_j + 1)]
+
+                    # evaluate neighbors
+                    left = cmp_left(curr, _curr, p)
+                    down = cmp_down(curr, _curr, p)
+                    right = cmp_right(curr, _curr, p)
+                    up = cmp_up(curr, _curr, p)
+
+                    l = edge(index(i, j, m), index(_i, _j, m), left, "r", "l")
+                    d = edge(index(i, j, m), index(_i, _j, m), down, "u", "d")
+                    r = edge(index(i, j, m), index(_i, _j, m), right, "l", "r")
+                    u = edge(index(i, j, m), index(_i, _j, m), up, "d", "u")
+
+                    edges.append((l.w, i, j, _i, _j, 0, l))
+                    edges.append((d.w, i, j, _i, _j, 1, d))
+                    edges.append((r.w, i, j, _i, _j, 2, r))
+                    edges.append((u.w, i, j, _i, _j, 3, u))
+
+    return edges
+
+def dfs(graph, grid, x, i, j, _i, _j, visited, LEFT, TOP, RIGHT, DOWN):
+    visited[_i][_j] = True
+
+    LEFT[0] = min(LEFT[0], j)
+    TOP[0] = min(TOP[0], i)
+    RIGHT[0] = max(RIGHT[0], j)
+    DOWN[0] = max(DOWN[0], i)
+
+    grid[p*i : p*(i+1), p*j : p*(j+1)] = x[p*_i : p*(_i+1), p*_j : p*(_j+1)]
+
+    for key, val in graph[_i][_j].children.items():
+        if not visited[val[0]][val[1]]:
+            d = direction(key)
+            dfs(graph, grid, x, i + d[1], j + d[0], val[0], val[1], visited, LEFT, TOP, RIGHT, DOWN)
+
+def bfs_refine(graph, grid, x, i, j, _i, _j, visited):
+    pass
+
+if __name__ == "__main__":
+    img_index = "1800"
+
+    # problem A
+    x = Image.open(img_index + ".png", 'r')
+    x = np.array(x)
+    m = 32
+    p = int(x.shape[0] / m)
+
+    edges_matrix = None
+    edges = get_edges(x, p, m, edges_matrix)
+    heapq.heapify(edges)
+
+    visited = {}
+
+    cost = 0
+    ans = []
+    while len(edges) > 0:
+        curr = heapq.heappop(edges)
+        w = curr[0]
+        i = curr[1]
+        j = curr[2]
+        _i = curr[3]
+        _j = curr[4]
+        pos = curr[6]
+
+        hash_1 = (i, j, pos.src)
+        hash_2 = (_i, _j, pos.dest)
+
+        if hash_1 in visited or hash_2 in visited:
+            if not hash_1 in visited:
+                visited[hash_1] = hash_2
+            if not hash_2 in visited:
+                visited[hash_2] = hash_1
+            continue
+
+        visited[hash_1] = hash_2
+        visited[hash_2] = hash_1
+
+        cost += w
+
+        ans.append(curr)
+
+    # print(len(ans))
+    # print(cost)
+
+    last = (m - 1, m - 1)
+
+    graph = [[None for j in range(m)] for i in range(m)]
+    for i in range(m):
+        for j in range(m):
+            graph[i][j] = cell(-1, -1)
+
+    visited = {}
+    visited[last] = last
+
+    while len(visited) < m * m:
+        for k in range(len(ans)):
+            curr = ans[k]
+            w = curr[0]
+            i = curr[1]
+            j = curr[2]
+            _i = curr[3]
+            _j = curr[4]
+            pos = curr[6]
+            # print(len(ans), i, j, _i, _j)
+
+            if (i, j) in visited:
+                if not (_i, _j) in visited:
+                    d = direction(pos.dest)
+                    visited[(_i, _j)] = (i, j, d)
+                    ans.pop(k)
+
+                    graph[i][j].children[pos.dest] = (_i, _j)
+                    graph[_i][_j].children[pos.src] = (i, j)
+
+                    break
+
+            elif (_i, _j) in visited:
+                if not (i, j) in visited:
+                    d = direction(pos.src)
+                    visited[(i, j)] = (_i, _j, d)
+                    ans.pop(k)
+
+                    graph[i][j].children[pos.dest] = (_i, _j)
+                    graph[_i][_j].children[pos.src] = (i, j)
+
+                    break
+
+    grid = np.copy(x)
+    grid = np.concatenate((grid, grid), axis=0)
+    grid = np.concatenate((grid, grid), axis=1)
+
+    LEFT = [inf]
+    TOP = [inf]
+    RIGHT = [inf]
+    DOWN = [inf]
+
+    visited = [[False for j in range(m)] for i in range(m)]
+    dfs(graph, grid, x, 7, 7, 7, 7, visited, LEFT, TOP, RIGHT, DOWN)
+
+    # for row in visited:
+    #     print(row)
+    # for i in range(m):
+    #     for j in range(m):
+    #         print(graph[i][j].children)
+            
+    #         if not visited[i][j]:
+    #             dfs(graph, grid, x, i, j, visited)
+
+    # print(ans)
+    # for key, val in visited.items():
+
+    grid = grid[p*TOP[0] : p*(TOP[0] + m), p*LEFT[0] : p*(LEFT[0] + m)]
+
+    img = Image.fromarray(grid, 'RGB')
+    img.save(img_index + "_grid.jpeg")
